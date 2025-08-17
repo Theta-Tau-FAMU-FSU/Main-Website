1bc630b7ceac2b89c1692923ed3289f89eb1ee41
Nicolas Azzi Main PC <github.thousand607@passmail.com>
2025-08-16 21:39:45 -0400

    Added accessablity to phones for the landing page

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/rush")
+def rush():
+    return render_template("rush.html")
+
+
 if __name__ == "__main__":
     app.logger.debug("Hi")
     app.run(debug=True)
